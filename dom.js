5d646ecd360752cf5422775334cc4ba46ddaa0f9
mayurjungari <mayurjungari1@gmail.com>
2023-07-31 00:26:45 +0530

    firstcomiit

---
+++ dom.js
@@ -0,0 +1,9 @@
+
+//console.log(document.getElementById('header-title'))
+var k=document.getElementById('header-title');
+k.style.borderBottom='8px solid black'
+var p=document.getElementById('aditem')
+p.style.color='green';
+p.style.fontWeight='bold';
+//k.style.backgroundColor='red'
+
