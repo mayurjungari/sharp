af3aa8853b5bf1746fdb671e5a5a3d97f61e757e
mayurjungari <mayurjungari1@gmail.com>
2023-07-31 10:16:30 +0530

    fontweight updated and 3rd li background

--- dom.js
+++ dom.js
@@ -1,9 +1,16 @@
 
 //console.log(document.getElementById('header-title'))
-var k=document.getElementById('header-title');
-k.style.borderBottom='8px solid black'
-var p=document.getElementById('aditem')
-p.style.color='green';
-p.style.fontWeight='bold';
+// var k=document.getElementById('header-title');
+// k.style.borderBottom='8px solid black'
+// var p=document.getElementById('aditem')
+// p.style.color='green';
+// p.style.fontWeight='bold';
 //k.style.backgroundColor='red'
+var item=document.getElementsByClassName('list-group-item');
+item[2].style.backgroundColor='green';
+for(var p=0;p<item.length;p++)
+{
+    item[p].style.fontWeight='bold';
+}
+
 
